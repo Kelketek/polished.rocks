cfdb9a47748777fd966777190f373c9593b0db57
Taylor Glenn <taylorglenn@live.com>
2021-07-17 08:31:07 -0500

    wip

---
+++ src/components/TheTumbler.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>ROCK TUMBLER, BITCH</h1>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TheTumbler'//,
+  // computed: {
+  //   nextStop () {
+  //     return new Date(this.$store.state.nextStop)
+  //   }
+  // }
+})
+
+// export const getSecondsUntilStageIsDone = (): number => {
+//   return 0
+// }
+
+</script>
---
+++ src/views/Tumbler.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <TheTumbler />
+  </v-container>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import TheTumbler from '../components/TheTumbler.vue'
+
+export default defineComponent({
+  name: 'Tumbler',
+  components: {
+    TheTumbler
+  }
+})
+</script>
