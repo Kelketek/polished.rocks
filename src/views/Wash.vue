01dcf51214728d584e10e45b68a3eaeb7d537612
Fox Danger Piacenti <fox@artconomy.com>
2021-07-17 08:55:58 -0500

    WIP

---
+++ src/views/Wash.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container>
+
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'Wash.vue',
+  created () {
+    if (this.$store.state.running) {
+      this.router.replace({ name: 'Placeholder' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
