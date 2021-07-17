f806372feceb833e5e7ea0539e6324a3173dac74
Corey Pon <cjames9001@gmail.com>
2021-07-17 17:43:55 -0500

    Adding functionality to the TropyCase

---
+++ src/views/Trophy.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <TrophyCase />
+  </v-container>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import TrophyCase from '../components/TrophyCase.vue'
+
+export default defineComponent({
+  name: 'Trophy',
+  components: {
+    TrophyCase
+  }
+})
+</script>
