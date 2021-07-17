f39a10b5604618e9a22894be262e375ee73591b0
Taylor Glenn <taylorglenn@live.com>
2021-07-17 08:51:52 -0500

    wip, committing so Fox can use my route

--- src/components/TheTumbler.vue
+++ src/components/TheTumbler.vue
@@ -1,17 +1,17 @@
 <template>
-  <h1>ROCK TUMBLER, BITCH</h1>
+  <h1>Next Stop: {{ nextStop }}</h1>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'TheTumbler'//,
-  // computed: {
-  //   nextStop () {
-  //     return new Date(this.$store.state.nextStop)
-  //   }
-  // }
+  name: 'TheTumbler',
+  computed: {
+    nextStop () {
+      return new Date(this.$store.state.nextStop)
+    }
+  }
 })
 
 // export const getSecondsUntilStageIsDone = (): number => {
