98d010f5c3cb83450d9f0d36f7deafcdc23fc8c5
Fox Danger Piacenti <fox@artconomy.com>
2021-07-18 08:08:54 -0500

    Remove symlink handling of rock assets-- doesn't work in Windows.

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import { POLISH_CYCLES } from '@/types/POLISH_CYCLES'
 export const ROCK_DATA = {
   [ROCK_TYPES.LAPIZ_LAZULI]: {
     assets: {
-      [POLISH_CYCLES.UNPOLISHED]: require('./assets/lapis_lazuli/unpolished.svg'),
+      [POLISH_CYCLES.UNPOLISHED]: require('./assets/unpolished1.svg'),
       [POLISH_CYCLES.COARSE]: require('./assets/lapis_lazuli/coarse.svg'),
       [POLISH_CYCLES.FINE]: require('./assets/lapis_lazuli/fine.svg'),
       [POLISH_CYCLES.PREPOLISH]: require('./assets/lapis_lazuli/prepolish.svg'),
@@ -14,7 +14,7 @@ export const ROCK_DATA = {
   },
   [ROCK_TYPES.JADE]: {
     assets: {
-      [POLISH_CYCLES.UNPOLISHED]: require('./assets/jade/unpolished.svg'),
+      [POLISH_CYCLES.UNPOLISHED]: require('./assets/unpolished1.svg'),
       [POLISH_CYCLES.COARSE]: require('./assets/jade/coarse.svg'),
       [POLISH_CYCLES.FINE]: require('./assets/jade/fine.svg'),
       [POLISH_CYCLES.PREPOLISH]: require('./assets/jade/prepolish.svg'),
@@ -23,7 +23,7 @@ export const ROCK_DATA = {
   },
   [ROCK_TYPES.OBSIDIAN]: {
     assets: {
-      [POLISH_CYCLES.UNPOLISHED]: require('./assets/obsidian/unpolished.svg'),
+      [POLISH_CYCLES.UNPOLISHED]: require('./assets/unpolished2.svg'),
       [POLISH_CYCLES.COARSE]: require('./assets/obsidian/coarse.svg'),
       [POLISH_CYCLES.FINE]: require('./assets/obsidian/fine.svg'),
       [POLISH_CYCLES.PREPOLISH]: require('./assets/obsidian/prepolish.svg'),
